7a4c81c07d8f212a75bdb2febb8114bc6d4d580a
jpeng11 <jianling.peng@monsanto.com>
2019-04-11 11:43:55 -0500

    second commit

--- run_analysis.r
+++ run_analysis.r
@@ -79,7 +79,9 @@ Final <- Combined %>% gather(key = "Measure_Type", value = "Measurement",
                              -c(Subject, Source, Activity_Description))
 Final_Average <- Final %>%
     group_by(Subject, Measure_Type, Activity_Description) %>%
-    summarise(Average= mean(Measurement, na.rm = T))
+    summarise(Average= mean(Measurement, na.rm = T)) %>%
+    spread(Measure_Type, Average)
+    
 
 write.table(Final_Average, file = "Final_Average.txt", row.names = F)
 write.table(Final, file = "Final.txt", row.names= F)
